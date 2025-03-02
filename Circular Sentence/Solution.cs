2ad79051255ddfe309d89ddf0ae0f9901e7d4967
MahmoudAmin <ma7moud888.amim@gmail.com>
2025-03-02 15:29:14 +0200

    New Problem Added

---
+++ Circular Sentence/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circular_Sentence
+{
+    public class Solution
+    {
+        public bool IsCircularSentence(string s)
+        {
+            for (int i = 0; i < s.Length-1; i++) {
+                if (s[i + 1] == ' ' && s[i + 2] != s[i])
+                    return false;
+            }
+            return s[s.Length - 1] == s[0];
+        }
+    }
+}
