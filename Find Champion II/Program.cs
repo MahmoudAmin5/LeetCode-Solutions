086b55a2ed7a6157610801cb97b11d1a3b680f6f
MahmoudAmin <ma7moud888.amim@gmail.com>
2025-03-16 07:18:04 +0200

    New Problem Added

---
+++ Find Champion II/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Find_Champion_II
+{
+    public class Solution
+    {
+        public int FindChampion(int n, int[][] edges)
+        {
+            int[] freq = new int[n];
+            int count = 0 , ans=-1;
+
+            for (int i = 0; i < edges.Length; i++) {
+                freq[edges[i][1]]++;
+            }
+            for (int i = 0; i < n; i++) { 
+               if(freq[i] == 0)
+                {
+                    count++;
+                    ans = i;
+                }
+            }
+            return (count == 1) ? ans : -1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    }
+
